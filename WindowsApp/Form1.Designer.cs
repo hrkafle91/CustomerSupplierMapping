87e186b98e2d1ab806993327df3d5bd8706eb2f3
Hemraj Kafle <kafleh@sheridancollege.ca>
2020-03-05 22:34:55 -0500

    The graph with lines between the nearest customer and supplier points created

---
+++ WindowsApp/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(917, 146);
+            this.btnShow.Margin = new System.Windows.Forms.Padding(6, 11, 6, 11);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(175, 84);
+            this.btnShow.TabIndex = 0;
+            this.btnShow.Text = "Show Graph";
+            this.btnShow.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(3844, 2097);
+            this.Margin = new System.Windows.Forms.Padding(6, 11, 6, 11);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ WindowsApp/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        System.Drawing.Graphics graphicsObj;
+        List<Point> customers;
+        List<Point> suppliers;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void ShowGraph()
+        {
+            
+            CreateRectangle();
+            customers = PopulateCustomers();
+            suppliers = PopulateSuppliers();
+            MapCustomerSupplier();
+            btnShow.Hide();
+            btnShow.Enabled=false;
+        }
+
+        private List<Point> PopulateSuppliers()
+        {
+            return new List<Point>()
+            {
+                new Point(200, 50),
+                new Point(250, 250),
+                new Point(325, 325),
+                new Point(450, 50),
+                new Point(425, 475)
+            };
+        }
+
+        private void CreateRectangle()
+        {
+            Pen pen = new Pen(Color.Black);
+            Rectangle rectangle = new Rectangle(0, 0, 600, 600);
+            graphicsObj.DrawRectangle(pen, rectangle);     
+        }
+
+        private void MapCustomerSupplier()
+        {
+            foreach (var cus in customers)
+            {
+                Point start = cus;
+                Point end = suppliers.First();
+
+                double minLength = GetLength(start, end);
+
+                foreach (var sup in suppliers)
+                {
+                    double length = GetLength(cus, sup);
+                    if (length < minLength)
+                    {
+                        end = sup;
+                        minLength = length;
+                    }
+                }
+                graphicsObj.DrawLine(GetPen(suppliers.IndexOf(end)), start, end);
+            }
+        }
+
+        private Pen GetPen(int index)
+        {
+            List<Color> colors = new List<Color>()
+            {
+                Color.Red, Color.Green, Color.Yellow, Color.Blue, Color.Gray
+            };
+
+            return new Pen(colors[index]);
+        }
+
+        private double GetLength(Point cus, Point sup)
+        {
+            return Math.Sqrt((SquareOf(sup.X - cus.X)) + SquareOf(sup.Y - cus.Y));
+        }
+
+        private int SquareOf(int a)
+        {
+            return a * a;
+        }
+
+        private List<Point> PopulateCustomers()
+        {
+            List<Point> points = new List<Point>();
+            Random random = new Random();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                points.Add(new Point(random.Next(0, 600), random.Next(0, 600)));
+            }
+            return points;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            graphicsObj = this.CreateGraphics();
+            ShowGraph();
+        }
+    }
+}
